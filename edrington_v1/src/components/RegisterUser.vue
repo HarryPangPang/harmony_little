185452e177af3dbd9599356b03739d8df4af2bc7
ZhangQiangQQ <1562055324@qq.com>
2018-10-19 18:40:36 +0800

    regx

--- edrington_v1/src/components/RegisterUser.vue
+++ edrington_v1/src/components/RegisterUser.vue
@@ -4,27 +4,38 @@
     <i class="el-icon-back goback_icon" @click="goback"></i>
     <img src="../assets/edrington.png" class="edrington_logo"/>
     <div class="register_wrap_2">
+      <el-form :model="employee_info" :rules="rules" ref="ruleForm">
         <el-row :gutter="10" :span="6" >
+
             <el-col :xs="23" :sm="23" :md="23" :lg="12" :xl="12" class="input_wrap">
-            <span>姓名：</span> 
-            <el-input v-model="employee_info.employee_name" placeholder="请输入姓名" style="width:80%"></el-input>
+              <el-form-item prop="rule_name">
+                <span>姓名：</span>
+                <el-input v-model="employee_info.employee_name" placeholder="请输入姓名" style="width:80%"></el-input>
+              </el-form-item>
             </el-col>
             
             <el-col :xs="23" :sm="23" :md="23" :lg="12" :xl="12" class="input_wrap"> 
+              <el-form-item prop="rule_mobile">
               <span>手机：</span>
               <el-input v-model="employee_info.employee_mobile" placeholder="请输入手机号" style="width:80%"></el-input>
+              </el-form-item>
             </el-col>
 
             <el-col :xs="23" :sm="23" :md="23" :lg="12" :xl="12" class="input_wrap">
+              <el-form-item prop="rule_email">
               <span>邮箱：</span>
               <el-input v-model="employee_info.employee_email" placeholder="请输入邮箱" style="width:80%"></el-input>
+              </el-form-item>
             </el-col>
 
             <el-col :xs="23" :sm="23" :md="23" :lg="12" :xl="12" class="input_wrap">
+              <el-form-item prop="rule_password">
               <span>密码：</span>
               <el-input v-model="employee_info.employee_password" placeholder="请输入密码" style="width:80%"></el-input>
+              </el-form-item>
             </el-col>
         </el-row>
+      </el-form>
     <div class="submit_reg">
        <el-button round @click="regist_now">注册</el-button>
     </div>
@@ -41,16 +52,67 @@ export default {
   },
   data () {
     return {
+      ruleForm:{
+          rule_name:'',
+          rule_mobile:'',
+          rule_email:'',
+          rule_password:''
+      },
       employee_info:{
         employee_name: '',
         employee_mobile:'',
         employee_email:'',
         employee_password:''
+      },
+      rules: {
+        rule_name: [
+          { required: true, message: '请输入姓名', trigger: 'blur' },
+          { min: 3, max: 20, message: '长度在 2 到 20个字符', trigger: 'blur' }
+        ],
+        rule_mobile: [
+          { required: true, message: '请输入手机号', trigger: 'change' }
+        ],
+        rule_email: [
+          { required: true, message: '请输入邮箱', trigger: 'change' }
+        ],
+        rule_password: [
+           { required: true, message: '请输入密码', trigger: 'change' }
+        ]
       }
     }
   },
   methods:{
     regist_now(){
+      var validateMobilePhone = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('负责人手机号不可为空'));
+        } else {
+          if (value !== '') { 
+            var reg=/^1[3456789]\d{9}$/;
+            if(!reg.test(value)){
+              callback(new Error('请输入有效的手机号码'));
+            }
+          }
+          callback();
+        }
+      };
+
+
+      var validateEmail = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请正确填写邮箱'));
+        } else {
+          if (value !== '') { 
+            var reg=/^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+            if(!reg.test(value)){
+              callback(new Error('请输入有效的邮箱'));
+            }
+          }
+          callback();
+        }
+      };
+
+
       let employee_info = {
         employee_name: this.employee_info.employee_name,
         employee_mobile:this.employee_info.employee_mobile,
